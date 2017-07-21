72108871af1773fe5e940cb1b4c33e62423ee967
Francesco Bonomi <frbonomi@gmail.com>
2017-07-21 23:49:09 +0200

    Move scraper to its own class and file

--- following.py
+++ following.py
@@ -2,14 +2,9 @@ from datetime import datetime
 import getpass
 import os
 import pickle
-import re
 import time
 
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from scraper import Scraper
 
 # Ask for input
 target = input('Enter the target username: ')
@@ -19,101 +14,28 @@ credentials = {
     'password': getpass.getpass()
 }
 
-# Go to Instagram
-chromedriver_path = 'drivers/chromedriver'
-driver = webdriver.Chrome(chromedriver_path)
-driver.get('https://www.instagram.com')
-
-# Go to log in
-login_link = WebDriverWait(driver, 3).until(
-    EC.presence_of_element_located((By.LINK_TEXT, 'Log in'))
-)
-login_link.click()
-
-# Authenticate
-username_input = driver.find_element_by_xpath('//input[@placeholder="Username"]')
-password_input = driver.find_element_by_xpath('//input[@placeholder="Password"]')
-
-username_input.send_keys(credentials['username'])
-password_input.send_keys(credentials['password'])
-password_input.send_keys(Keys.RETURN)
-time.sleep(1)
-
-# Go to user profile and click following
-driver.get('https://www.instagram.com/%s/' % target)
-following_link = WebDriverWait(driver, 3).until(
-    EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, 'following'))
-)
-following_link.click()
-expected_number = int(re.search('(\d+)', following_link.text).group(1))
-time.sleep(1)
-
-# Select scrollable users div
-users_list_container = driver.find_element_by_xpath(
-    '//div[@role="dialog"]//ul/parent::div'
-)
-
-# Get the actual list of `li` elements
-def get_updated_user_list(index = None):
-    users = users_list_container.find_elements(By.XPATH, 'ul//li')
-    if index == None:
-        return users
-    return users[index:]
-
-# Scroll an element
-def scroll(element, times = 1):
-    while times > 0:
-        driver.execute_script(
-            'arguments[0].scrollTop = arguments[0].scrollHeight',
-            element
-        )
-        time.sleep(.2)
-        times -= 1
-
-# While there are more users scroll and save the results
-links = []
-last_user_index = 0
-updated_list = get_updated_user_list()
-initial_loading_speed = 5
-
-while updated_list[last_user_index] is not updated_list[-1]:
-    scroll(users_list_container, 5)
-
-    for index, user in enumerate(updated_list):
-        if index < last_user_index:
-            continue
-
-        try:
-            link_to_user = user.find_element(By.TAG_NAME, 'a').get_attribute('href')
-            last_user_index = index
-            if link_to_user not in links:
-                links.append(link_to_user)
-                print(link_to_user)
-        except:
-            if (initial_loading_speed > 1):
-                initial_loading_speed -= 1
-            pass
-
-
-    updated_list = get_updated_user_list()
+scraper = Scraper(target)
+scraper.authenticate(credentials['username'], credentials['password'])
+users = scraper.get_users('following', verbose = True)
+scraper.close()
 
 # Store the scraped following users link
 date = datetime.fromtimestamp(time.time()).strftime('%Y-%m-%dT%H:%M:%S')
 db_path = 'exports/%s/following%s.pkl' % (target, date)
 os.makedirs(os.path.dirname(db_path), exist_ok = True)
-pickle.dump(links, open(db_path, 'wb'))
+pickle.dump(users, open(db_path, 'wb'))
 
 # Stats
-print('Number of followers: %i' % len(links))
-if len(links) < expected_number:
-    mean_users = expected_number - len(links)
+print('Number of followers: %i' % len(users))
+if len(users) < scraper.expected_number:
+    mean_users = scraper.expected_number - len(users)
     print('Expected %i followers but only found %i. %i %s probably blocked you.'
           % (
-            expected_number,
-            len(links),
+            scraper.expected_number,
+            len(users),
             mean_users,
             'people' if mean_users != 1 else 'person'
         )
     )
 
-driver.close()
+
---
+++ scraper.py
@@ -0,0 +1,103 @@
+import re
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class Scraper(object):
+    def __init__(self, username):
+        self.target = username
+        self.driver = webdriver.Chrome('drivers/chromedriver')
+
+    def close(self):
+        self.driver.close()
+
+    def authenticate(self, username, password):
+        print('\nLogging in…')
+        self.driver.get('https://www.instagram.com')
+
+        # Go to log in
+        login_link = WebDriverWait(self.driver, 3).until(
+            EC.presence_of_element_located((By.LINK_TEXT, 'Log in'))
+        )
+        login_link.click()
+
+        # Authenticate
+        username_input = self.driver.find_element_by_xpath(
+            '//input[@placeholder="Username"]'
+        )
+        password_input = self.driver.find_element_by_xpath(
+            '//input[@placeholder="Password"]'
+        )
+
+        username_input.send_keys(username)
+        password_input.send_keys(password)
+        password_input.send_keys(Keys.RETURN)
+        time.sleep(1)
+
+    def get_users(self, group, verbose = False):
+        print('\nGetting users…')
+        self._open_dialog(group)
+
+        links = []
+        last_user_index = 0
+        updated_list = self._get_updated_user_list()
+        initial_scrolling_speed = 5
+
+        # While there are more users scroll and save the results
+        while updated_list[last_user_index] is not updated_list[-1]:
+            self._scroll(self.users_list_container, 5)
+
+            for index, user in enumerate(updated_list):
+                if index < last_user_index:
+                    continue
+
+                try:
+                    link_to_user = user.find_element(By.TAG_NAME, 'a').get_attribute('href')
+                    last_user_index = index
+                    if link_to_user not in links:
+                        links.append(link_to_user)
+                        if verbose == True:
+                            print(link_to_user)
+                except:
+                    if (initial_scrolling_speed > 1):
+                        initial_scrolling_speed -= 1
+                    pass
+
+            updated_list = self._get_updated_user_list()
+        return links
+
+    def _open_dialog(self, group):
+        print('\nNavigating to %s profile…' % self.target)
+        self._go_to_user_profile(self.target)
+        dialog_link = WebDriverWait(self.driver, 3).until(
+            EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, group))
+        )
+        dialog_link.click()
+        self.expected_number = int(
+            re.search('(\d+)', dialog_link.text).group(1)
+        )
+        time.sleep(1)
+        self.users_list_container = self.driver.find_element_by_xpath(
+            '//div[@role="dialog"]//ul/parent::div'
+        )
+
+    def _go_to_user_profile(self, username):
+        self.driver.get('https://www.instagram.com/%s/' % username)
+
+    # Get the actual list of `li` elements containing the users info
+    def _get_updated_user_list(self):
+        return self.users_list_container.find_elements(By.XPATH, 'ul//li')
+
+    # Scroll an element
+    def _scroll(self, element, times = 1):
+        while times > 0:
+            self.driver.execute_script(
+                'arguments[0].scrollTop = arguments[0].scrollHeight',
+                element
+            )
+            time.sleep(.2)
+            times -= 1

19fae2af2e98ccd004f14c27e0381fd4feb17830
Francesco Bonomi <frbonomi@gmail.com>
2017-07-22 03:01:18 +0200

    Refactor ask_input and add multiple option input

--- following.py
+++ following.py
@@ -1,14 +1,14 @@
 import file_io
 from scraper import Scraper
-from utils import ask_input
+from utils import ask_input, ask_multiple_option
 
 
 # Ask for input
 target = ask_input('Enter the target username: ')
+group = ask_multiple_option(options = ['followers', 'following']);
 print('\nEnter your Instagram credentials')
 username = ask_input('Username: ')
 password = ask_input(is_password = True)
-group = 'following';
 
 scraper = Scraper(target)
 scraper.authenticate(username, password)
--- utils.py
+++ utils.py
@@ -3,8 +3,24 @@
 import getpass
 
 
-def ask_input(message = '', is_password = False):
-    answer = ''
-    while answer == '':
-        answer = getpass.getpass() if is_password == True else input(message)
-    return answer
+def ask_input(prompt = '', is_password = False):
+    while True:
+        answer = getpass.getpass() if is_password == True else input(prompt)
+        if answer is not '':
+            return answer
+
+
+def ask_multiple_option(options, prefix = 'Choose between', prompt = ': '):
+    def exists(index):
+        return 0 <= index < len(options)
+
+    # while answer == None or not exists(answer):
+    while True:
+        print(prefix)
+        for index, option in enumerate(options):
+            print('  {} - {}'.format(index + 1, option))
+        answer = input(prompt).strip()
+        if answer is not '':
+            index = int(answer) - 1
+            if exists(index):
+                return options[index]
